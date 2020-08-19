c1f2ca5d1ae6192ca24f5b826bdff423e66e90de
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-08-19 07:10:08 +0530

    Basic Layout Setup

--- src/components/Sidebar/Sidebar.component.js
+++ src/components/Sidebar/Sidebar.component.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
 import "./Sidebar.style.css";
+
 function Sidebar() {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+            <ChatIcon />
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
 }
 
 export default Sidebar;
