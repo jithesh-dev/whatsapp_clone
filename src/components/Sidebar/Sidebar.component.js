15f79520537e956bbe08db36c11914c3f73ef991
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-08-19 06:42:19 +0530

    First Commit �

---
+++ src/components/Sidebar/Sidebar.component.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Sidebar.style.css";
+function Sidebar() {
+  return <div className="sidebar"></div>;
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDrD9QAGakarpLsqHyPBiMMCdOpwmjLfmk",
+  authDomain: "whatsapp-clone-109b1.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-109b1.firebaseio.com",
+  projectId: "whatsapp-clone-109b1",
+  storageBucket: "whatsapp-clone-109b1.appspot.com",
+  messagingSenderId: "726758700256",
+  appId: "1:726758700256:web:f381c9bbe6dd133d5f8c85",
+  measurementId: "G-M4Y1PL73T5",
+};
