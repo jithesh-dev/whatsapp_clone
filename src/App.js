ceaf4a9363165e0654ec0441c457181b31912a72
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-08-21 12:14:08 +0530

    Logout Functionality completed with session retention

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 
-import './App.css';
-import Sidebar from './components/Sidebar/Sidebar.component';
-import Chat from './components/Chat/Chat.component';
-import Login from './pages/Login/Login.pages';
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar.component";
+import Chat from "./components/Chat/Chat.component";
+import Login from "./pages/Login/Login.pages";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useStateValue } from './StateProvider/StateProvider';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./StateProvider/StateProvider";
+import { auth } from "./firebase";
+import { actionTypes } from "./reducer/reducer";
 function App() {
-  const [{ user }] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
+  const [isLoading, setisLoading] = useState(true);
+
   useEffect(() => {
-    console.log(user);
-  }, [user]);
+    const unsubscribe = auth.onAuthStateChanged((usr) => {
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: usr,
+      });
+      setisLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    console.log("Is Loading", isLoading);
+  }, [isLoading]);
 
   return (
-    <div className='app'>
-      {!user ? (
-        <Login />
-      ) : (
-        <div className='app__body'>
-          <Router>
-            <Sidebar />
-            <Switch>
-              <Route path='/rooms/:roomId'>
-                <Chat />
-              </Route>
-              <Route path='/'>
-                <Chat />
-              </Route>
-            </Switch>
-          </Router>
-        </div>
-      )}
+    <div className="app">
+      {!isLoading &&
+        (!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Router>
+              <Sidebar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <Chat />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        ))}
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.component.js
+++ src/components/Sidebar/Sidebar.component.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Avatar, IconButton } from '@material-ui/core';
-import DonutLargeIcon from '@material-ui/icons/DonutLarge';
-import ChatIcon from '@material-ui/icons/Chat';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
-import SidebarChat from '../SidebarChat/SidebarChat.component';
-import db from '../../firebase';
-import './Sidebar.style.css';
-import { useStateValue } from '../../StateProvider/StateProvider';
+import React, { useState, useEffect } from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "../SidebarChat/SidebarChat.component";
+import db from "../../firebase";
+import "./Sidebar.style.css";
+import { useStateValue } from "../../StateProvider/StateProvider";
+import { auth } from "../../firebase";
 
 function Sidebar() {
   const [{ user }, dispatch] = useStateValue();
   const [rooms, setRooms] = useState([]);
   useEffect(() => {
-    const unsubscribe = db.collection('rooms').onSnapshot((snapshot) =>
+    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
       setRooms(
         snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -25,28 +26,28 @@ function Sidebar() {
   }, []);
 
   return (
-    <div className='sidebar'>
-      <div className='sidebar__header'>
+    <div className="sidebar">
+      <div className="sidebar__header">
         <Avatar src={user?.photoURL} />
-        <div className='sidebar__headerRight'>
+        <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
           </IconButton>
           <IconButton>
             <ChatIcon />
           </IconButton>
-          <IconButton>
-            <MoreVertIcon />
+          <IconButton onClick={() => auth.signOut()}>
+            <PowerSettingsNewIcon />
           </IconButton>
         </div>
       </div>
-      <div className='sidebar__search'>
-        <div className='sidebar__searchContainer'>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
           <SearchOutlinedIcon />
-          <input placeholder='Search or start new chat' type='text' />
+          <input placeholder="Search or start new chat" type="text" />
         </div>
       </div>
-      <div className='sidebar__chats'>
+      <div className="sidebar__chats">
         <SidebarChat addNewChat />
         {rooms.map((room) => (
           <SidebarChat key={room.id} id={room.id} name={room.data.name} />
